8614838236c51166104cc1fc994ab5660d31bd6c
Rahhuul <rahulrayineni@gmail.com>
2025-12-05 09:30:54 -0800

    Initial code for Quiz Service

---
+++ src/main/java/com/quiz/demo/QuizQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizQuestionsApplication.class, args);
+	}
+
+}
