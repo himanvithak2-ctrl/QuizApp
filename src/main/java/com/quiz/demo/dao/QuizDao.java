41c07b9b02ce1935023cc99beff6b69ea7548f68
himmanvitha <himanvithak2@gmail.com>
2025-12-26 05:52:02 +0530

    Quiz Application

---
+++ src/main/java/com/quiz/demo/dao/QuizDao.java
@@ -0,0 +1,12 @@
+package com.quiz.demo.dao;
+
+
+import com.quiz.demo.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+
+
+}
